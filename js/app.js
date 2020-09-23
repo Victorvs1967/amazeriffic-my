2355b9b1b4822bdd93018ab07935a4603b2903b3
Victor <victorsmirnov@me.com>
2020-09-23 13:33:20 +0300

    create tags subsystem

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
-const main = () => {
+const organizedByTags = (todos) => {
+    const tags = [];
+    todos.forEach((todo) => {
+        todo.tags.forEach((tag) => {
+            if (tags.indexOf(tag) === -1) {
+                tags.push(tag);
+            }
+        });
+    });
+    const tagObjects = tags.map((tag) => {
+        const toDoWishTag = [];
+        todos.forEach((todo) => {
+            if (todo.tags.indexOf(tag) !== -1) {
+                toDoWishTag.push(todo.description);
+            }
+        });
+        return {"name": tag, "toDos": toDoWishTag};
+    });
+    return tagObjects;
+};
+
+const main = (tasks) => {
     'use strict';
-    const toDos = [
-        'Bay food',
-        'Renew few tasks',
-        'Prepare to lecture on Monday',
-        'Answer to letters',
-        'Walk with Gracy to park',
-        'End to read book'
-    ];
+    let toDos = tasks.map((toDo) => {
+        return toDo.description;
+    });
     $('.tabs a span').toArray().forEach((tab) => {
         $(tab).on('click', () => {
             $('.tabs a span').removeClass('active');
@@ -26,13 +42,35 @@ const main = () => {
                 });
                 $('main .content').append($content);
             } else if ($(tab).parent().is(':nth-child(3)')) {
+
+                const organizedByTag = organizedByTags(tasks);
+
+                organizedByTag.forEach((tag) => {
+                    const $tagName = $('<h4>').text(tag.name),
+                        $content = $('<ul>');
+                    tag.toDos.forEach((description) => {
+                        $content.append($('<li>').text(description));
+                    });
+                    $('main .content').append($tagName);
+                    $('main .content').append($content);
+                });
+                $('main .content').append($content);
+            } else if ($(tab).parent().is(':nth-child(4)')) {
                 const $content = $('<div>');
-                $content.append($('<input>'));
+                $content.append($('<p>').text('Description'));
+                $content.append($('<input>').addClass('task'));
+                $content.append($('<p>').text('Tags'));
+                $content.append($('<input>').addClass('tags'));
                 $content.append($('<button>').text('Enter'));
+
                 const addFromInput = () => {
-                    if ($('.content input').val() !== '') {
-                        toDos.push($('.content input').val());
-                        $('.content input').val('');    
+                    if ($('.content input.task').val() !== '' && $('.content input.tags').val() !== '') {
+                        tasks.push({'description': $('.content input.task').val(), 'tags': $('.content input.tags').val().split(','),});
+                        toDos = tasks.map((todo) => {
+                            return todo.description;
+                        });
+                        $('.content input.task').val('');    
+                        $('.content input.tags').val('');    
                     }        
                 };
                 $('main .content').append($content);
@@ -42,7 +80,7 @@ const main = () => {
                 $('.content input').on('keypress', (event) => {
                     if (event.keyCode === 13) {
                         addFromInput();
-                    }
+                        }
                 });
             }
         });        
@@ -50,4 +88,8 @@ const main = () => {
     });
     return false;
 };
-$(document).ready(main);
+$(document).ready(() => {
+    $.getJSON('./data/todo.json', (toDos) => {
+        main(toDos);
+    });
+});
